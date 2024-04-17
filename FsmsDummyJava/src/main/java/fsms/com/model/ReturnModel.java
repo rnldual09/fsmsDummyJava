22764c46a4ecfcc58b7a1c8a42138b1305745721
rnldual09 <rnldual09@naver.com>
2024-04-17 09:40:37 +0900

    전자정부 swagger 테스트

--- FsmsDummyJava/src/main/java/fsms/com/model/ReturnModel.java
+++ FsmsDummyJava/src/main/java/fsms/com/model/ReturnModel.java
@@ -4,12 +4,21 @@ import java.util.HashMap;
 import java.util.Map;
 import egovframework.com.cmm.EgovMessageSource;
 import fsms.com.util.BeanUtil;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
+@ApiModel(value="리턴모델입니다")
 public class ReturnModel {
 	
 	private EgovMessageSource egovMessageSource = (EgovMessageSource) BeanUtil.getBean("egovMessageSource");
+	
+	@ApiModelProperty(value="설명1", example="예시1", required=true)
 	private Map<String,Object> content = new HashMap<>();
+	
+	@ApiModelProperty(value="설명2", example="예시2", required=false)
 	private String codeAndMessage;
+	
+	@ApiModelProperty(value="설명3", example="예시3", required=false)
 	private String code;
 	
 	public Map<String, Object> getContent() {
--- FsmsDummyJava/src/main/java/fsms/com/web/TestController.java
+++ FsmsDummyJava/src/main/java/fsms/com/web/TestController.java
@@ -11,7 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import fsms.com.model.ReturnModel;
 import fsms.com.service.TestService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
+@Api(tags = {"Test Controller 입니다(**대제목)"}, description = "API 서비스 예시(**소제목)")
 @RestController
 public class TestController {
 	
@@ -23,6 +30,31 @@ public class TestController {
 		String test = testService.testConnection();
 		System.out.println("test : " + test);
 	}
+	
+	@ApiOperation(value = "axios 통신테스트(**사용목적)", notes = "front에서 axios로 호출 시 잘 작동 되는지 테스트를 위하여 작성(**설명)")
+//	@ApiImplicitParams({
+//            @ApiImplicitParam(
+//                name = "변수명1"
+//                , value = "변수값1"
+//                , required = false
+//                , dataType = "string"
+//                , paramType = "path"
+//                , defaultValue = "None"
+//            )
+//        ,
+//            @ApiImplicitParam(
+//                name = "변수명2"
+//                , value = "변수값2"
+//                , required = false
+//                , dataType = "string"
+//                , paramType = "query"
+//                , defaultValue = ""
+//            )
+//        })
+	@ApiResponses({
+        @ApiResponse(code=200, message="성공"),
+        @ApiResponse(code=401, message="접근 권한이 없습니다.")
+	})
 	
 	@PostMapping(value = "/test/testAxios")
 	public ReturnModel testAxios(@RequestBody Map<String,Object> TaxiMap) throws Exception {
